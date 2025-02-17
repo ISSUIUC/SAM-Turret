607184c2024880aeb799b32ebf54483bf17a224b
14Million <kulasik2@illinois.edu>
2025-02-17 14:30:24 -0600

    Changed controller input of dt to time and added variables to later implement motor outputs

--- Controller.py
+++ Controller.py
@@ -10,18 +10,18 @@ class PID:
         self.prev_error = 0
         self.integral = 0
 
-
-    def compute(self, error, dt):
+    def compute(self, error, step_size, dt):
         self.integral += error
         derivative = (error - self.prev_error)/dt
         PID_calc = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
         self.prev_error = error
-        return PID_calc
-
-
+        return PID_calc * step_size
 
 class Controller:
     def __init__(self, turret_lat, turret_long, turret_alt):
+        # Initialize time
+        self.prev_time = 0
+
         # Input turret's GPS position (degrees)
         self.turret_x, self.turret_y, self.turret_z = Controller.GPS_to_ECEF(turret_lat, turret_long, turret_alt)
 
@@ -33,8 +33,17 @@ class Controller:
         Kp_pitch = 1; Ki_pitch = 1; Kd_pitch = 1
         self.pitch_pid = PID(Kp_pitch, Ki_pitch, Kd_pitch)
 
+        # Initialize motor step size (degrees) and time between steps (s)
+        self.yaw_step_size = 1
+        self.yaw_step_time = 1
+        self.pitch_step_size = 1
+        self.pitch_step_time = 1
 
-    def controller(self, rocket_lat, rocket_long, rocket_alt, current_yaw, current_pitch, dt):
+        # Convert degrees to radians
+        self.yaw_step_size *= math.pi/180
+        self.pitch_step_size *= math.pi/180
+        
+    def controller(self, rocket_lat, rocket_long, rocket_alt, current_yaw, current_pitch, time):
         # Input rocket's GPS position (degrees), current yaw of turret (degrees), curret pitch of turret (degrees)
 
         # Get relative coordinates by converting to ECEF and subtracting the turret's position
@@ -55,17 +64,20 @@ class Controller:
         yaw_error = target_yaw - current_yaw
         pitch_error = target_pitch - current_pitch
 
+        # Calculate dt
+        dt = time - self.prev_time
+        self.prev_time = time
+
         # Calculate control values
-        yaw_control = self.yaw_pid.compute(yaw_error, dt)
-        pitch_control = self.pitch_pid.compute(pitch_error, dt)
+        yaw_steps = self.yaw_pid.compute(yaw_error, self.yaw_step_size, dt)
+        pitch_steps = self.pitch_pid.compute(pitch_error, self.pitch_step_size, dt)
 
         # Convert to motor input
-        self.motor_input(yaw_control, pitch_control)
+        self.motor_input(yaw_steps, pitch_steps)
 
-
-    def motor_input(self, yaw, pitch):
+    def motor_input(self, yaw_steps, pitch_steps):
+        # Take in number of steps and excute those steps
         pass
-
 
     @staticmethod
     def GPS_to_ECEF(lat, long, alt): 
@@ -85,4 +97,3 @@ class Controller:
         y = (N + alt) * np.cos(lat) * np.sin(long)
         z = ((1 - e2) * N + alt) * np.sin(lat)
         return x, y, z
-        
